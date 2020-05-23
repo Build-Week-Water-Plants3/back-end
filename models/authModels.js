ff11ea3b6b1819d1ed9696e9460a4677d100221d
Darnea A. Pinkett <msdarneapinkett@gmail.com>
2020-05-23 12:13:29 -0400

    authmodels

---
+++ models/authModels.js
@@ -0,0 +1,27 @@
+const db = require('../data/dbconfig');
+
+function findById(id) {
+    return db('users')
+    .where({id})
+    .first()
+}
+
+async function add(user) {
+    const [id] = await db('users').insert(user);
+    return findById(id);
+}
+
+function find() {
+    return db('users').select('id', 'username', 'password');
+}
+
+function findyBy(filter) {
+    return db('users').where(filter);
+}
+
+module.exports = {
+    add,
+    find,
+    findyBy,
+    findById
+}
