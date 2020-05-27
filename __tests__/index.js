a36d4e42983c1546ecde0a3a51e3cd0e2d92bfe5
Darnea A. Pinkett <msdarneapinkett@gmail.com>
2020-05-27 16:32:20 -0400

    auth test

---
+++ __tests__/index.js
@@ -0,0 +1,17 @@
+const supertest = require("supertest");
+const server = require("../index");
+
+describe('GET/ on server', () => {
+    it('return status 200', async () => {
+        const res = await supertest(server).get('/');
+        expect(res.status).toBe(200);
+    });
+    it('return json', async () => {
+        const res = await supertest(server).get('/');
+        expect(res.type).toBe('application/json');
+    });
+    it('return Welcome to the server!', async () => {
+        const res = await supertest(server).get('/');
+        expect(res.body).toEqual('Welcome to the server!')
+    })
+})
